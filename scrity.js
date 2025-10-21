ae56e64c5234e868777de180005b460698638bb9
ArseneMerci <harsenemerci@gmail.com>
2025-10-21 15:34:16 +0200

    js update

--- scrity.js
+++ scrity.js
@@ -11,8 +11,8 @@ function blurring(){
         clearInterval(int)
     }
     loadText.innerHTML=`${load}%`
-    loadText.computedStyleMap.opacity = scale(load,0,100,1,0)
-    bg.computedStyleMap.filter = `blur(${scale(load,0,100,30,0)}px)`
+    loadText.style.opacity = scale(load,0,100,1,0)
+    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
